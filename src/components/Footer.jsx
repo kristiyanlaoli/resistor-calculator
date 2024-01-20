8f18fe92a6bb97d38eb985948695fe9bff594971
Kristiyan <kristiyanlaoli@gmail.com>
2024-01-20 16:55:41 +0700

    update footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,16 +1,18 @@
+import SvgVisit from "./SvgVisit";
+
 const Footer = () => {
   return (
     <>
-      <p className="font-medium text-sm text-slate-600 text-center">
-        Coded with ❤️ by{" "}
+      <p className="flex justify-center items-center font-medium text-sm text-slate-600 text-center">
+        Coded with ❤️ by
         <a
           href="https://imkris.vercel.app"
           target="blank"
-          className="text-green-500 font-bold py-4 hover:text-red-400 hover:font-bold"
+          className="flex items-center pl-2 text-green-500 font-bold py-4 hover:text-red-400 hover:font-bold"
         >
           Kristiyan Laoli
+          <SvgVisit />
         </a>
-        .
       </p>
     </>
   );
